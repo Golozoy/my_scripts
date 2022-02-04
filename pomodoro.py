871748a16e72f7e2b58146a5c1f07ed0c60ba5e4
q <q@db.localhoct>
2022-02-04 17:18:01 +1000

    Changes to be committed: 	modified:   README.md - add description of scripts 	new file:   pomodoro.py - implementation of the tomato method

---
+++ pomodoro.py
@@ -0,0 +1,22 @@
+import os
+import time
+
+
+def gen_notification(text: str='', title: str='Pomodoro')-> None:
+    os.system(f'notify-send -t {5*60*1000} {title} {text}')
+
+def main() -> None:
+    counter = 0
+    while True:
+        try:
+            gen_notification(text=f'{counter+1}_помидор_начался')
+            time.sleep(25*60)
+            counter += 1
+            gen_notification(text=f'Перерыв_{"15" if counter % 4 == 0 else "5"}_минут')
+            time.sleep(15*60 if counter % 4 == 0 else 5*60)
+        except KeyboardInterrupt:
+            gen_notification(text=f'Выполнено_{counter}_помидора')
+            break
+
+if __name__ == "__main__":
+    main()
