cbf6435c3c4bbe9697318a338ce451a3cafb77a0
q <my@mail.org>
2022-05-28 18:58:48 +1000

    edit uppip with asyncio

--- uppip.py
+++ uppip.py
@@ -1,48 +1,43 @@
 #!/usr/bin/python3
 
 
+import asyncio
 import os
 import subprocess
+import time
 
 
-def main() -> None:
-    print('\npip upgrade\n')
-    os.system("pip3 install --upgrade pip")
-    print('\ndone\n')
-    print('\nall packeges upgrade\n')
-    package = get_package()
-    upgrade_package(package)
-    pprint()
-    print("done")
+async def main() -> None:
+    start_time = time.time()
+    print('pip upgrade')
+    await upgrade_package('pip')
+    print('pip is up to date')
+    print('all packeges upgrade')
+    package = await get_package()
+    await asyncio.gather(*[upgrade_package(pack[0]) for pack in package.items()])
+    await asyncio.sleep(0)
+    await pprint()
+    print("all packages is up to date")
+    print(f'time is: {time.time() - start_time}')
 
-def get_package() -> dict:
+async def get_package() -> dict:
     """
     Метод возращает словарь установленных пакетов.
     Где ключ - названия пакета. Значения его версия
     """
-    package = dict()
     key = list()
     val = list()
     temp_list = subprocess.check_output(f"pip3 list", shell=True).decode().split()
-    for el in range(len(temp_list)):
-        if el % 2 == 0:
-            key.append(temp_list[el])
-        else:
-            val.append(temp_list[el])
-    for el in range(len(key)):
-        package[key[el]] = val[el]
-    return package
+    for el in range(4, len(temp_list)):
+        key.append(temp_list[el]) if el % 2 == 0 else val.append(temp_list[el])
+    return dict(zip(key, val))
 
-def pprint() -> None:
-    """
-    Выводит список всех установленых пакетов и их версии
-    """
+async def pprint() -> None:
     os.system("pip3 list")
 
-def upgrade_package(packege: dict) -> None:
-    for itm in packege.items():
-        os.system(f"pip3 install --upgrade {itm[0]}")
+async def upgrade_package(package):
+    subprocess.check_output(f"pip3 install --upgrade {package}", shell=True)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
